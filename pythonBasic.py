1fb7d5efed0c22c2469fd9b84d85987362745453
oryxx <hongbing.tan@gmail.com>
2016-07-14 23:36:12 +0800

    learn numpy

--- pythonBasic.py
+++ pythonBasic.py
@@ -1,9 +1,12 @@
-def test():
-    """Stupid test function"""
-    l = [i for i in range(100)]
-if __name__ == '__main__':
-    import timeit
-    print(timeit.timeit("test()", setup="from __main__ import test"))
+import pandas as pd
+import numpy as np
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print(df)
+a = 10
+b = 10
+c = np.array([1, 2, 3])
+d = a + b
 
 
 
